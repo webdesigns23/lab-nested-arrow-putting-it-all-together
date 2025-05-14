606a843ab702d623dccb634dbe47df496c107e53
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-14 08:33:51 -0600

    Added Login Tracker

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+//Initialize login feature, create user info:
+function createLoginTracker() {
+    const userInfo = {
+     username:"sharmaine",
+     password:"correctpw"
+     }
+    let attemptCount = 0;
 
+    //arrow function to handle login attempts:
+    const loginAttempt = (passwordAttempt) => {
+        attemptCount++;
 
+        if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+            return "Login Successful!";
+        } else if (attemptCount <= 3) {
+            return `Attempt ${attemptCount}: Login Failed!`;
+        } else {
+            return "Account locked due to too many failed login attempts!";
+        }
+    };
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+    return loginAttempt;
+}
+
+const login = createLoginTracker();
+
+//Test password attempt
+console.log(login("1234"));
+console.log(login("5678"));
+console.log(login("correctpw"));
+console.log(login("pw4"));
+
